908ae2d4c038b6161d4a74ddaec96bf575daccb3
Maks <unreal9981@gmail.com>
2024-06-21 22:12:28 +0300

    wip

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import bot from "./bot.js";
 import setupHandlers from "./handlers.js";
-import menuHandlers from "./handlers/menuHandlers.js";
-
+import { authorization } from './database/api.js';
 import { userMenu, adminMenu } from './UI/menus.js';
 
 class ProxyUABot {
@@ -16,18 +15,21 @@ class ProxyUABot {
         this.bot.onText(/\/start/, (msg) => {
             const chatId = msg.chat.id;
             const userFirstName = msg.from.first_name;
-            
+            console.log("===start==")
             authorization(chatId).then(data => {
                 if (data) {
                     this.userData = data;
                     if (data.type === 'admin') {
+                        console.log("===data.type==", data.type)
                         this.bot.sendMessage(chatId, `Привіт ${userFirstName}\nГоловне меню:`, adminMenu);
                       } else {
+                        console.log("===data.type=Привіт=")
                         this.bot.sendMessage(chatId, `Привіт ${userFirstName}\nГоловне меню:`, userMenu);
                       }
                 } else {
                     addNewUser(msg).then(data => {
                         this.userData = data;
+                        console.log("===data.type=Привіт=", this.userData)
                         this.bot.sendMessage(chatId, `Привіт ${userFirstName}\nГоловне меню:`, userMenu);
                     })
                 }
