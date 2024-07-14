93dc3985c2f8a4c61b42f518fb285c16c8465d64
Maks <unreal9981@gmail.com>
2024-07-14 14:18:33 +0300

    wip

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-// const TOKEN = "7097258304:AAEydDFxb6LIrM490ltTd1F0eJGnrjPF-tw";
-const TOKEN = "7256165099:AAGhnOJOLxHtnK_emJvFoQxZWrXPmxumTp8";
+const TOKEN = "7097258304:AAEydDFxb6LIrM490ltTd1F0eJGnrjPF-tw";
+// const TOKEN = "7256165099:AAGhnOJOLxHtnK_emJvFoQxZWrXPmxumTp8";
 
 export { TOKEN };
